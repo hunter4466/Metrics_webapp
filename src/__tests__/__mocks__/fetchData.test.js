8d44e3e6e07588c809ba3232133f07e155e059f9
Mario Chois <mario.chois.navarro@gmail.com>
2021-09-07 22:15:38 -0500

    Testing: Set up integration test with mocked http request on fetch

--- src/__tests__/__mocks__/fetchData.test.js
+++ src/__tests__/__mocks__/fetchData.test.js
@@ -1,15 +1,13 @@
 import { fetchData } from '../../redux/home/home';
 
-global.fetch = jest.fn(() => Promise.resolve({
-  json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
-}));
-
 beforeEach(() => {
-  fetch.mockClear();
+  fetch.resetMocks();
 });
 
-it('Fetch function works correctly', async () => {
-  const response = await fetchData();
-  expect(response).toEqual({ rates: { CAD: 1.42 } });
-  expect(fetch).toHaveBeenCalledTimes(1);
+describe('Home component', () => {
+  it('Fetch function works correctly', async () => {
+    fetch.mockResponseOnce(JSON.stringify({ Mock: 'MockedData' }));
+    const response = await fetchData();
+    expect(response).toEqual({ Mock: 'MockedData' });
+  });
 });
